7d463756dc2c3a46f466080a18f8b293d1af83dc
t.kosmopoulos <t.kosmopoulos@athexgroup.gr>
2021-04-09 00:36:37 +0300

    feat: display landmark position in google map

--- src/app/modules/landmarks/landmarks.module.ts
+++ src/app/modules/landmarks/landmarks.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { LandmarksRoutingModule } from './landmarks-routing.module';
 import { LandmarksComponent } from './components/landmarks/landmarks.component';
 import { LandmarkDetailsComponent } from './components/landmark-details/landmark-details.component';
+import { AgmCoreModule } from '@agm/core';
 
 
 @NgModule({
   declarations: [LandmarksComponent, LandmarkDetailsComponent],
   imports: [
     CommonModule,
-    LandmarksRoutingModule
+    LandmarksRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAxUsvOZIxk8Tnm9UsbLJvU8on1aXZsRho'
+    })
   ]
 })
 export class LandmarksModule { }
