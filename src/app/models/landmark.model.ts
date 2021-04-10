2070a27b62a8ee2cb5d090ad3bc9b54b48921c10
t.kosmopoulos <t.kosmopoulos@athexgroup.gr>
2021-04-10 20:20:26 +0300

    refactor: extend landmark interface

--- src/app/models/landmark.model.ts
+++ src/app/models/landmark.model.ts
@@ -1,3 +1,13 @@
 export interface Landmark {
+  objectId: string;
   title: string;
+  short_info: string;
+  description: string;
+  location: number[];
+  order: number;
+  url: string;
+  photo: any;
+  photo_thumb: any;
+  createdAt: string;
+  updatedAt: string;
 }
