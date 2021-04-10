49e16125bbdc856392b6c2afcb3b9c6db248b665
t.kosmopoulos <t.kosmopoulos@athexgroup.gr>
2021-04-10 22:37:05 +0300

    feat: implement user login

--- src/app/modules/user/components/login/login.component.ts
+++ src/app/modules/user/components/login/login.component.ts
@@ -3,6 +3,9 @@ import { ApiUserService } from '@app/services/api';
 import { Utilities } from '@app/common/utilities';
 import { GetLoginResponse } from '@app/models/api-user.model';
 import { AppVariables } from '@app/common/app-variables';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Constants } from '@app/common/constants';
 
 @Component({
   selector: 'app-login',
@@ -11,19 +14,35 @@ import { AppVariables } from '@app/common/app-variables';
 })
 export class LoginComponent implements OnInit {
 
-  username = 'admin';
-  password = 'admin';
+  loginForm: FormGroup;
+  loginErrorMessage = '';
 
-  constructor(private apiUserService: ApiUserService) { }
+  constructor(private apiUserService: ApiUserService, private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
-    this.apiUserService.login(this.username, this.password)
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login(): void {
+    this.apiUserService.login(this.formControls.username.value, this.formControls.password.value)
       .subscribe((loginResponse: GetLoginResponse) => {
         if (Utilities.isDefined(loginResponse.sessionToken)) {
           AppVariables.username = loginResponse.username;
           AppVariables.userSessionToken = loginResponse.sessionToken;
+          this.router.navigateByUrl('');
+        } else {
+          this.loginErrorMessage = 'Authentication failed.';
         }
+      },
+      (err) => {
+        this.loginErrorMessage = 'Authentication failed.';
+        Utilities.logMsg(err.message, Constants.logLevel.error);
       });
   }
+
+  get formControls(): any { return this.loginForm.controls; }
 
 }
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     UserRoutingModule
   ]
 })
